d3f0de4b71d7ec504537bbecdf9739cd17e6c6dd
Philou <pfouque@gmail.com>
2020-12-04 23:54:18 +0100

    v1.00 starters

---
+++ readme.md
@@ -0,0 +1,11 @@
+ionic start
+select:
+framework : angular
+name: ionic-sandbox
+(..)
+
+cd ionic-sndbox
+code .
+ionic serve --no-open --port=8100
+
+
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'tab-map',
+        loadChildren: () => import('../tab-map/tab-map.module').then(m => m.TabMapPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
