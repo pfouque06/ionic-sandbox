c46d9745a2f765393dba31f647e7be8826ebc030
Philou <pfouque@gmail.com>
2021-01-31 00:32:47 +0100

    v1.61 sandbox : add ionic rename & reorder

--- src/app/core/pages/themes/sandbox/sandbox.page.ts
+++ src/app/core/pages/themes/sandbox/sandbox.page.ts
@@ -47,15 +47,37 @@ export class SandboxPage implements OnInit {
   ];
 
   // Ionic reorder group
-  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
-  handlerEnabled = true;
-  bucket = [
+  // @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
+  @ViewChild('reorder01') reorderGroup01: IonReorderGroup;
+  reorderDisabled01 = false;
+  handlerEnabled01 = true;
+  bucket01 = [
     'item 1',
     'item 2',
     'item 3',
     'item 4',
     'item 5',
   ];
+
+  @ViewChild('reorderGroup02') reorderGroup02: IonReorderGroup;
+  bucket02 = [
+    'element 1',
+    'element 2',
+    'element 3',
+    'element 4',
+    'element 5',
+  ];
+  tempBucket02: any[];
+
+  // @ViewChild('reorderGroup03') reorderGroup03: IonReorderGroup;
+  // bucket03 = [
+  //   'card 1',
+  //   'card 2',
+  //   'card 3',
+  //   'card 4',
+  //   'card 5',
+  //   'card 6',
+  // ];
 
   constructor(private _formBuilder: FormBuilder) { console.log('themes/sandbox'); }
 
@@ -67,6 +89,9 @@ export class SandboxPage implements OnInit {
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+
+    // Ionic Reorder & rename
+    this.tempBucket02 = this.bucket02.map((item) => ({ name: item, focus: false }));
   }
 
   // Material Tabs
@@ -96,20 +121,37 @@ export class SandboxPage implements OnInit {
   // public closeAllExpansionPanels() { this.accordion.closeAll(); }
 
   // Material Drag & Drop
-  drop(event: CdkDragDrop<string[]>) {
+  public drop(event: CdkDragDrop<string[]>) {
     if (event.previousIndex < 0 || event.previousIndex >= this.items.length) { return; }
     if (event.currentIndex < 0 || event.currentIndex >= this.items.length) { return; }
-    console.log('Mat Drag: Before complete', this.items);
     // move item in item from previousIndex to currentIndex);
     this.items.splice(event.currentIndex, 0, this.items.splice(event.previousIndex, 1)[0]);
     console.log('Mat Drag: After complete', this.items);
   }
 
   // Ionic reorder group
-  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
-    console.log('Ionic Reorder: Before complete', this.bucket);
+  public doReorder01(ev: CustomEvent<ItemReorderEventDetail>) {
     // Update the items variable to the new order of items
-    this.bucket = ev.detail.complete(this.bucket);
-    console.log('Ionic Reorder: After complete', this.bucket);
+    this.bucket01 = ev.detail.complete(this.bucket01);
+    console.log('Ionic Reorder01: After complete', this.bucket01);
   }
+  toggleReorderGroup01() {
+    this.reorderDisabled01 = !this.reorderDisabled01;
+    this.reorderGroup01.disabled = this.reorderDisabled01;
+  }
+
+  public doReorder02(ev: CustomEvent<ItemReorderEventDetail>) {
+    // Update the items variable to the new order of items
+    this.tempBucket02 = ev.detail.complete(this.tempBucket02);
+    console.log('Ionic Reorder02: After complete', this.tempBucket02);
+  }
+  public reorder02Display(item: any) {
+    console.log('item.name: ', item.name);
+  }
+
+  // public doReorder03(ev: CustomEvent<ItemReorderEventDetail>) {
+  //   // Update the items variable to the new order of items
+  //   this.bucket03 = ev.detail.complete(this.bucket03);
+  //   console.log('Ionic Reorder03 After complete', this.bucket03);
+  // }
 }
