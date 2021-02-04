32ee83dde0402fcd25c822fbc6d90a976b6a5ee0
Philou <pfouque@gmail.com>
2021-02-05 00:49:25 +0100

    v1.68 add swUpdate check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -11,10 +11,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
+    private swUpdate: SwUpdate,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+          // TODO: Make a nice popup
+          if(confirm("New version available! Load New Version?")) {
+              window.location.reload();
+          }
+      });
+      // Check every 30s if a new version has been released.
+      // interval(30000).subscribe( () => this.swUpdate.checkForUpdate() );
+    }
     this.initializeApp();
   }
 
