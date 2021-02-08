719eebcce58288412416d6b3daef795fa545bae6
Philou <pfouque@gmail.com>
2021-02-08 02:06:34 +0100

    v1.74 attempt to improve QRcode Scan

--- src/app/core/pages/themes/goodies/qrcode-scan/qrcode-scan.page.ts
+++ src/app/core/pages/themes/goodies/qrcode-scan/qrcode-scan.page.ts
@@ -1,23 +1,21 @@
-import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import jsQR from 'jsqr';
 
 export const QRSCAN_PAGE = '/tabs/themes/qrcode-scan';
+
 @Component({
   selector: 'app-qrcode-scan',
   templateUrl: './qrcode-scan.page.html',
   styleUrls: ['./qrcode-scan.page.scss'],
 })
-export class QRcodeScanPage implements OnInit {
+export class QRcodeScanPage implements OnInit, AfterViewInit, OnDestroy {
 
+  public pageUrl: string;
   public qrCode: string = '';
-  public scanActive = false;
-  public scanFound = false;
-  public notFound = false;
-  public timeout = false;
-  public wrong = false;
-  public failed = false;
+  public scanActive: boolean = false;
+  public scanFound: boolean = false;
 
   @ViewChild('video', {static: false }) video: ElementRef;
   @ViewChild('canvas', {static: false }) canvas: ElementRef;
@@ -35,19 +33,27 @@ export class QRcodeScanPage implements OnInit {
       console.log('I am a PWA iOS !');
     }
   }
-
-  ngOnInit() {
+  
+  public ngOnInit() {
+    console.log('ngOnInit().url: ', this.router.url);
+    this.pageUrl = this.router.url || QRSCAN_PAGE;
   }
-
-  ngAfterViewInit() {
+  
+  public ngAfterViewInit() {
     this.videoElement = this.video.nativeElement;
     this.canvasElement = this.canvas.nativeElement;
     this.canvasContext  = this.canvasElement.getContext('2d');
-
-    this.startScan();
   }
 
-  async startScan() {
+  public ngOnDestroy() {
+    console.log('ngOnDestroy() --> release Vdeo stream');
+    this.releaseVideo()
+  }
+  
+  public async startScan() {
+    console.log('startScan()');
+    // debugger;
+    
     // reset Scan
     this.resetScan();
     // start video stream
@@ -59,67 +65,80 @@ export class QRcodeScanPage implements OnInit {
     this.videoElement.play();
     this.scanActive = true;
     // refresh page
-    this.ref.markForCheck();
+    // this.ref.markForCheck();
     // start scanning process
     requestAnimationFrame(this.scan.bind(this));
   }
-
-  async scan() {
+  
+  public async scan() {
     // check that active page is still this one and stop, release video if not the case
-    if ( this.router.url !== QRSCAN_PAGE) {
+    if ( this.router.url !== this.pageUrl) {
+      console.log(`url has moved to ${this.router.url}, stopping worthless scan! --> should clear video stream onDestroy`);
       this.stopScan();
     }
-
-    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
-      if (this.scanActive) {
+    
+    if (this.scanActive) {
+      if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
         // capture image from video stream
         this.canvasElement.height = this.videoElement.videoHeight;
         this.canvasElement.width = this.videoElement.videoWidth;
         this.canvasContext.drawImage(this.videoElement, 0, 0, this.canvasElement.height, this.canvasElement.width);
         const imageData = this.canvasContext.getImageData(0, 0, this.canvasElement.height, this.canvasElement.width);
+        console.log('scan() - process Image scanning');
         // scan captured image
-        const scanResult = jsQR(
-          imageData.data,
-          imageData.width,
-          imageData.height,
-          { inversionAttempts: 'dontInvert'}
-          );
-
+        const scanResult = jsQR( imageData.data, imageData.width, imageData.height, { inversionAttempts: 'dontInvert'} );
+          
         if (! scanResult) {
+          console.log('scan() - no result yet');
           // restart image capture
           requestAnimationFrame(this.scan.bind(this));
         } else {
+          console.log('scan() --> QR code found!');
           this.stopScan();
           this.getScanResult(scanResult.data);
         }
-      }
-    } else {
-      if (this.scanActive) {
+      } else {
+        console.log('scan() - video stream not ready yet');
         requestAnimationFrame(this.scan.bind(this));
       }
     }
   }
 
-  stopScan() {
+  public stopScan() {
     if (this.scanActive) {
+      console.log('stopScan()');
       this.scanActive = false;
       this.videoElement.pause();
       // refresh page
-      this.ref.markForCheck();
+      // this.ref.markForCheck();
     }
   }
 
-  resetScan() {
-      this.scanFound = false;
-      this.notFound = false;
-      this.timeout = false;
-      this.failed = false;
-      this.wrong = false;
-      // refresh page
-      this.ref.markForCheck();
+  public resetScan() {
+    console.log('resetScan()');
+    this.scanFound = false;
+    // this.notFound = false;
+    // this.timeout = false;
+    // this.failed = false;
+    // this.wrong = false;
+    
+    // refresh page
+    // this.ref.markForCheck();
+  }
+  
+
+  public releaseVideo() {
+    console.log('releaseVideo()');
+    if (this.scanActive) { this.videoElement.pause(); }
+    delete this.videoElement.srcObject      // The call mentioned in other answers
+    this.videoElement.removeAttribute('srcObject'); // empty source
+    this.videoElement.load(); // empty buffer
+    this.videoElement.remove()    // Removing the video element altogether
   }
 
   public getScanResult(qrCode: string) {
+    console.log(`getScanResult(${qrCode})`);
+    this.scanFound = true;
     this.qrCode = qrCode
   }
 }
