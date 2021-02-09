390570f7110334620bc914beb1824247b6ffbcf4
Philou <pfouque@gmail.com>
2021-02-09 22:45:28 +0100

    v1.77 add interval for sw update request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { SwUpdate } from '@angular/service-worker';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,13 +19,12 @@ export class AppComponent {
   ) {
     if (this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
-          // TODO: Make a nice popup
-          if(confirm("New version available! Load New Version?")) {
-              window.location.reload();
-          }
+        if(confirm("New version available!\nLoad New Version?")) {
+            window.location.reload();
+        }
       });
       // Check every 30s if a new version has been released.
-      // interval(30000).subscribe( () => this.swUpdate.checkForUpdate() );
+      interval(30000).subscribe( () => this.swUpdate.checkForUpdate() );
     }
     this.initializeApp();
   }
