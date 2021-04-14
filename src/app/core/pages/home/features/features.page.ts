f512aa1e28b69aa03ea62071c52e58b1f44babdc
philou <pfouque@gmail.com>
2021-04-14 20:01:26 +0200

    v1.83 home/features: add koa swagger and repo

--- src/app/core/pages/home/features/features.page.ts
+++ src/app/core/pages/home/features/features.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'koa-services';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AuthService, SERVER_ADDRESS, SERVER_PROTOCOL } from 'koa-services';
 
 @Component({
   selector: 'app-features',
@@ -8,7 +8,15 @@ import { AuthService } from 'koa-services';
 })
 export class FeaturesPage {
 
-  constructor(private authService: AuthService) { console.log('home/features'); }
+  public swagger: string;
+
+  constructor(
+    @Inject(SERVER_ADDRESS) public readonly serverAddress: string,
+    @Inject(SERVER_PROTOCOL) public readonly serverProtocol: 's' | '',
+    private authService: AuthService) { 
+    console.log('home/features');
+    this.swagger = `http${this.serverProtocol}://${this.serverAddress}/swagger`;
+  }
 
   public ping(): boolean {
     return this.authService.pong;
