3df4380d67445658837348299b1f910224b8b04a
philou <pfouque@gmail.com>
2021-04-23 19:31:03 +0200

    v2.10 complete Gesture bench

--- src/app/shared/templates/components/themes/ionic-gestures/ionic-gestures.component.ts
+++ src/app/shared/templates/components/themes/ionic-gestures/ionic-gestures.component.ts
@@ -1,5 +1,11 @@
-import { AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, NgZone, OnInit, ViewChild } from '@angular/core';
 import { GestureController, DomController } from '@ionic/angular';
+import { timer } from 'rxjs';
+import { timeout } from 'rxjs/operators';
+
+type positionType = { x: number, y: number};
+const styleBackLight = `background: var(--ion-color-light)`;
+const styleBackNeutral = `background:`;
 
 @Component({
   selector: 'app-ionic-gestures',
@@ -8,15 +14,30 @@ import { GestureController, DomController } from '@ionic/angular';
 })
 export class IonicGesturesComponent implements OnInit, AfterViewInit {
 
+  // Double Tap
   @ViewChild('doubleTapBox', { read: ElementRef }) private doubleTapBox: ElementRef;
+  private doubleTapInterval = 300;
+  private lastTapTimestamp = 0;
+  private tapCount = 0;
+  public doubleTapStatus = false;
+
+  // Long Press
   @ViewChild('longPressBox', { read: ElementRef }) private longPressBox: ElementRef;
+  private longPressInterval = 251;
+  private longPressStartTimestamp = 0;
+  private longPressActive = false;
+  public longPressStatus = false;
+  public power = 0;
+
+  // Drag Gesture
   @ViewChild('box', { read: ElementRef }) private box: ElementRef;
   @ViewChild('dragBox', { read: ElementRef }) private dragBox: ElementRef;
-  public power = 0;
-  public longPressActive = false;
+  private startPosition: positionType;
+  public currentPosition: positionType = {x: 0, y: 0};
 
-  public deltaX = 0;
-  public deltaY = 0;
+  // Swipe Gesture
+  @ViewChild('SwipeBox', { read: ElementRef }) private SwipeBox: ElementRef;
+  public swipeStatus = false;
 
   constructor(private gestureCtrl: GestureController, private domCtrl: DomController, private zoneCtrl: NgZone, ) { }
 
@@ -25,88 +46,180 @@ export class IonicGesturesComponent implements OnInit, AfterViewInit {
   async ngAfterViewInit() {
     // Use DomCtrl to get the correct header height
     // await this.domCtrl.read(() => {
+    // console.log(this.dragBox.nativeElement.offsetHeight);
+    // console.log(this.dragBox.nativeElement.offsetWidth);
+    // console.log(this.dragBox.nativeElement.clientHeight);
+    // console.log(this.dragBox.nativeElement.clientWidth);
+    // console.log(this.dragBox.nativeElement.getBoundingClientRect());
+    // console.log(this.dragBox.nativeElement.getClientRects());
+    // console.log(this.box.nativeElement.getBoundingClientRect());
+    // console.log(this.box.nativeElement.getClientRects());
     // });
-    const dragBoxHeight = this.dragBox.nativeElement.offsetHeight;
-    const dragBoxWidth = this.dragBox.nativeElement.offsetWidth;
-    const dragBoxInnerHeight = this.dragBox.nativeElement.clientHeight;
-    const dragBoxInnerWidth = this.dragBox.nativeElement.clientWidth;
-    console.log(this.dragBox.nativeElement.offsetHeight);
-    console.log(this.dragBox.nativeElement.offsetWidth);
-    console.log(this.dragBox.nativeElement.clientHeight);
-    console.log(this.dragBox.nativeElement.clientWidth);
-    console.log(this.dragBox.nativeElement.getBoundingClientRect());
-    console.log(this.dragBox.nativeElement.getClientRects());
-    console.log(this.box.nativeElement.getBoundingClientRect());
-    console.log(this.box.nativeElement.getClientRects());
-    const boxPosition = this.box.nativeElement.getBoundingClientRect();
-    this.setupGesture();
+    this.setupLongPressGestures();
+    this.setupDragGestures();
+    this.setSwipeGesture();
   }
 
-  // setupGesture(offsetHeight, offsetWidth, clientHeight, clientWidth) {
-  setupGesture() {
-    const doubleTabGesture = this.gestureCtrl.create({
-      gestureName: 'doubleTab',
-      el: this.doubleTapBox.nativeElement,
-      threshold: 0,
-      onStart: ev => {
-        console.log('doubleTabGesture start!', ev);
-      },
-      onEnd: ev => {
-        console.log('doubleTabGesture end!', ev);
-      }
-    }, true); // Run the gesture callback inside of NgZone!
-    doubleTabGesture.enable(true);
+  // tap manager
+  // @HostListener('click', ['$event'])
+  public handleTap(ev) {
+    // console.log('--> handleTap()', ev);
+    const tapTimestamp = Math.floor(ev.timeStamp);
+    const isDoubleTap = (this.lastTapTimestamp + this.doubleTapInterval ) > tapTimestamp;
+    // if ((!this.tapGesture.enabled && !this.doubleTapGesture.enabled) || this.isPressing || this.isMoving) { return this.resetTaps(); }
+    this.tapCount++;
+    this.lastTapTimestamp = tapTimestamp;
+    // console.log('tapTimestamp: ', tapTimestamp);
+    // console.log('isDoubleTap: ', isDoubleTap);
+    // console.log('tapCount: ', this.tapCount);
+    if (isDoubleTap) { this.emitTap(); }
+    else if ( this.tapCount > 1 ) {this.resetTaps(); }
+  }
 
+  // emit tap action
+  private emitTap() {
+    switch (this.tapCount) {
+      // case 1, 3, ... : this.handleOtherTap(); break;
+      case 2 : {
+        this.handleDoubleTap();
+        break;
+      }
+    }
+    this.resetTaps();
+  }
+
+  private handleDoubleTap() {
+    // console.log('--> handleDoubleTap');
+    this.doubleTapStatus = true;
+    this.doubleTapBox.nativeElement.style = styleBackLight;
+    timer(400).subscribe( () => {
+      this.doubleTapStatus = false;
+      this.doubleTapBox.nativeElement.style = styleBackNeutral;
+    } );
+  }
+
+  private resetTaps() {
+    this.tapCount = 0;
+    this.lastTapTimestamp = 0;
+    this.doubleTapStatus = false;
+  }
+
+  // Long press gesture setup
+  private setupLongPressGestures() {
     const longPressGesture = this.gestureCtrl.create({
       gestureName: 'longPress',
       el: this.longPressBox.nativeElement,
       threshold: 0,
       onStart: ev => {
+        // console.log('--> longPressGesture start', ev);
+        this.longPressStartTimestamp = Math.floor(ev.event.timeStamp);
         this.longPressActive = true;
+        this.longPressStatus = false;
         this.power = 0;
+        // console.log('pressStartTimestamp: ', this.pressStartTimestamp);
         this.increasePower();
       },
       onEnd: ev => {
+        // console.log('--> longPressGesture end', ev);
         this.longPressActive = false;
+        const pressEndTimestamp = Math.floor(ev.event.timeStamp);
+        const isLongPress = (this.longPressStartTimestamp + this.longPressInterval ) < pressEndTimestamp;
+        // console.log('pressEndTimestamp: ', pressEndTimestamp);
+        // console.log('isLongPress: ', isLongPress);
+        if (isLongPress) { this.handleLongPress(); }
       }
     }, true); // Run the gesture callback inside of NgZone!
     longPressGesture.enable(true);
+  }
 
+  private increasePower(timeout = 200) {
+    setTimeout(() => {
+      if (this.longPressActive) {
+          this.power++;
+          this.increasePower(timeout / 1.5);
+      }
+    }, timeout);
+  }
+
+  private handleLongPress() {
+    // console.log('--> handleLongPress');
+    this.longPressStatus = true;
+    this.longPressBox.nativeElement.style = styleBackLight;
+    timer(400).subscribe( () => {
+      this.longPressStatus = false;
+      this.longPressBox.nativeElement.style = styleBackNeutral;
+    } );
+  }
+
+  // drag gesture setup
+  private setupDragGestures() {
     const dragGesture = this.gestureCtrl.create({
       gestureName: 'drag',
       el: this.box.nativeElement,
       threshold: 0,
       onStart: ev => {
-        // console.log('dragGesture start!', this.deltaX, this.deltaY);
+        this.startPosition = { ...this.currentPosition};
+        // console.log('--> dragGesture start', this.startPosition);
       },
       onMove: ev => {
         // this.zoneCtrl.run(() => { });
-        const deltaX = this.deltaX + ev.deltaX;
-        const deltaY = this.deltaY + ev.deltaY;
-        this.box.nativeElement.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
-        // console.log('dragGesture move!', deltaX, deltaY);
+        this.dragBoxOnPosition({ x: this.startPosition.x + ev.deltaX, y: this.startPosition.y + ev.deltaY});
+        // console.log('--> dragGesture move', this.currentPosition);
       },
       onEnd: ev => {
         // this.zoneCtrl.run(() => { });
-        this.deltaX += ev.deltaX;
-        this.deltaY += ev.deltaY;
-        this.box.nativeElement.style.transform = `translate(${this.deltaX}px, ${this.deltaY}px)`;
-        // console.log('dragGesture end!', this.deltaX, this.deltaY);
+        this.dragBoxOnPosition({ x: this.startPosition.x + ev.deltaX, y: this.startPosition.y + ev.deltaY});
+        // console.log('--> dragGesture end', this.currentPosition);
       }
     }, true); // Run the gesture callback inside of NgZone!
     dragGesture.enable(true);
   }
 
-  increasePower(timeout = 200) {
-    setTimeout(() => {
-      if (this.longPressActive) {
-          // if (this.power >= 5) {
-          //   this.longPressActive = false;
-          //   return;
-          // }
-          this.power++;
-          this.increasePower(timeout / 1.5);
+  private dragBoxOnPosition(position: positionType) {
+    this.currentPosition = { ... position};
+    this.box.nativeElement.style.transform = `translate(${position.x}px, ${position.y}px)`;
+  }
+
+  public resetBoxPosition() {
+    this.dragBoxOnPosition( {x: 0, y: 0});
+  }
+
+  // Swipe Gesture setup
+  private setSwipeGesture() {
+    const swipeGesture = this.gestureCtrl.create({
+      gestureName: 'swipe',
+      el: this.SwipeBox.nativeElement,
+      threshold: 15,
+      direction: 'x',
+      onStart: ev => { },
+      onMove: ev => {
+        // console.log('--> swipeGesture move');
+        this.handleSwipeGesture(ev);
+      },
+      onEnd: ev => {
+        // console.log('--> swipeGesture end');
+        this.handleSwipeNavigate(ev);
       }
-    }, timeout);
+    }, true); // Run the gesture callback inside of NgZone!
+    swipeGesture.enable(true);
+  }
+
+  private handleSwipeGesture(ev) {
+    let delta = ev.deltaX;
+    delta = Math.min(Math.max(delta, -30), 30);
+    this.SwipeBox.nativeElement.style.transform = `translateX(${delta}px)`;
+  }
+
+  private handleSwipeNavigate(ev) {
+    this.SwipeBox.nativeElement.style.transform = `translateX(0px)`;
+    const delta = ev.deltaX;
+    if (delta > 100 || delta < -100) {
+      this.swipeStatus = true;
+      this.SwipeBox.nativeElement.style = styleBackLight;
+      timer(400).subscribe( () => {
+        this.swipeStatus = false;
+        this.SwipeBox.nativeElement.style = styleBackNeutral;
+      } );
+    }
   }
 }
